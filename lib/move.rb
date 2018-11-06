ef9d33e20b5a6c0ad7c295ec57caa80371166fcc
Teresa De Jesus <teresa1594@gmail.com>
2018-11-06 18:13:22 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index
+  puts "Where would you like to go?"
+  input_to_index = gets.strip
+  display_board(board)
+end
+
+
